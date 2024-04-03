4b331469f3a469348774a43704beb225d0a95133
Justin Hurstwright <jryanhurst@gmail.com>
2024-04-02 21:31:28 -0500

    start parsing out character

--- rakelib/ast_dump_parser.rb
+++ rakelib/ast_dump_parser.rb
@@ -1,13 +1,39 @@
+# frozen_string_literal: true
+
 require 'json'
 
 class AstDumpParser
   # @param [String] dump - the raw output from `clang -ast-dump=json`
-  def self.from_clang_dump(dump) 
+  def self.from_clang_dump(dump)
     new(JSON.parse(dump))
   end
 
-
   def initialize(ast_hash)
     @ast_hash = ast_hash
+  end
+
+  def parse!
+    @ast_hash['inner'].select { |a| api?(a) }.map { |d| parse(d) }
+  end
+
+  def parse(decl)
+    case decl['kind']
+    when 'RecordDecl'
+      puts "Struct #{decl['name']}"
+    when 'EnumDecl'
+      puts "Enum #{decl['name']}"
+    when 'FunctionDecl'
+      puts "Func #{decl['name']}"
+    end
+  end
+
+  def parse_struct(decl)
+  end
+
+  def api?(decl)
+    return true if decl['name']&.include?('mrb')
+    return true if decl['kind'] == 'EnumDecl'
+
+    false
   end
 end
--- tests/ast_dump_parser_test.rb
+++ tests/ast_dump_parser_test.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rakelib/ast_dump_parser'
 
 class TestAstDumpParser < Minitest::Test
   def test_with_a_perfect_square
-    assert ::AstDumpParser.from_clang_dump("{}")
+    refute_nil ::AstDumpParser.from_clang_dump('{}')
   end
 
-  def test_inner
+  def test_global_types
     file = File.open('tests/main_c_dump.json')
     parser = ::AstDumpParser.from_clang_dump(file.read)
     refute_nil file.size
     refute_nil parser
 
-
+    parser.parse!
   end
 end
